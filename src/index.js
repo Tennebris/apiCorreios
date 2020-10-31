fdfaf9e4cdff92a9a47cf699414cb3e00b6c68ce
Alisson santos <craftgames.alysson@gmail.com>
2020-10-31 14:08:01 -0300

    fix: heroku procfile

--- src/index.js
+++ src/index.js
@@ -5,16 +5,20 @@ const app = express();
 
 app.use(express.json());
 
-app.get("/rastreio", async (req, res) => {
-    try{
-    	const {id} = req.query;
-    	const result = await rastrearEncomendas([id]);
-	console.log(result["0"]);
-   	res.json(result["0"]);
-    }catch(e){
-	res.json({error: e});	
-    }
-})
+// app.get("/rastreio", async (req, res) => {
+//     try{
+//     	const {id} = req.query;
+//     	const result = await rastrearEncomendas([id]);
+// 	console.log(result["0"]);
+//    	res.json(result["0"]);
+//     }catch(e){
+// 	res.json({error: e});	
+//     }
+// });
+
+app.get("/",(req,res) => {
+    res.send("hello word");
+});
 
 app.listen(process.env.PORT || 3333,() => {
     console.log("iniciado");
